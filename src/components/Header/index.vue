185f74afc334a3a454679ac402a64b960b33512b
Anmizi <1845513904@qq.com>
2022-01-27 11:49:56 +0800

    完成路由组件的搭建

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -9,8 +9,8 @@
             <p>尚品汇欢迎您！</p>
             <p>
               <span>请</span>
-              <a href="###">登录</a>
-              <a href="###" class="register">免费注册</a>
+              <router-link to="/login">登录</router-link>
+              <router-link to="/register" class="register">免费注册</router-link>
             </p>
           </div>
           <div class="typeList">
@@ -28,9 +28,9 @@
       <!--头部第二行 搜索区域-->
       <div class="bottom">
         <h1 class="logoArea">
-          <a class="logo" title="尚品汇" href="###" target="_blank">
+          <router-link class="logo" title="尚品汇" to="/home" target="_blank">
             <img src="./images/logo.png" alt="" />
-          </a>
+          </router-link>
         </h1>
         <div class="searchArea">
           <form action="###" class="searchForm">
@@ -39,7 +39,7 @@
               id="autocomplete"
               class="input-error input-xxlarge"
             />
-            <button class="sui-btn btn-xlarge btn-danger" type="button">
+            <button class="sui-btn btn-xlarge btn-danger" type="button" @click="goSearch">
               搜索
             </button>
           </form>
@@ -52,6 +52,11 @@
 <script>
 export default {
   name: "Header",
+  methods: {
+    goSearch(){
+      this.$router.push('/search')
+    }
+  }
 };
 </script>
 
