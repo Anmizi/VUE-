ce0119be18f58177d41922252e86cb227562f4e0
Anmizi <1845513904@qq.com>
2022-01-27 22:15:29 +0800

    全局注册三级联动组件

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// 导入TypeNav组件
+import TypeNav from './pages/Home/TypeNav'
+Vue.component(TypeNav.name,TypeNav)
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,13 +1,12 @@
 <template>
-  <div>Home</div>
+  <TypeNav/>
 </template>
 
 <script>
 export default {
-  name: 'Home'
-}
+  name: "Home",
+};
 </script>
 
 <style lang="less" scoped>
-
 </style>
