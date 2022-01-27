e327b5692a5f42f260a84f741a0e88ea10ad8b9d
Anmizi <1845513904@qq.com>
2022-01-27 21:50:44 +0800

    :bug: 重写push和replace方法解决重复跳转同一路由报错问题

--- src/router/index.js
+++ src/router/index.js
@@ -7,24 +7,49 @@ import Home from '@/pages/Home'
 import Search from '@/pages/Search'
 import Login from '@/pages/Login'
 import Register from '@/pages/Register'
+// 保存VueRouter原型对象的push,replace方法
+let originPush = VueRouter.prototype.push
+let originReplace = VueRouter.prototype.replace
+// 重写push和replace方法
+VueRouter.prototype.push = function (location, resolve, reject) {
+  if (resolve && reject) {
+    originPush.call(this, location, resolve, reject)
+  } else {
+    originPush.call(this, location, () => {}, () => {})
+  }
+}
+VueRouter.prototype.replace = function (location, resolve, reject) {
+  if (resolve && reject) {
+    originReplace.call(this, location, resolve, reject)
+  } else {
+    originReplace.call(this, location, () => { }, () => { })
+  }
+}
+
 // 配置路由
 export default new VueRouter({
   routes: [
     {
       path: '/home',
-      component: Home
+      component: Home,
+      meta: { show: true }
     },
     {
-      path: '/search',
-      component: Search
+      name: 'search',
+      path: '/search/:keyword?',
+      component: Search,
+      meta: { show: true },
+      props: true
     },
     {
       path: '/login',
-      component: Login
+      component: Login,
+      meta: { show: false }
     },
     {
       path: '/register',
-      component: Register
+      component: Register,
+      meta: { show: false }
     },
     {
       path: '/',
