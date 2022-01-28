57ab02d4ed8204242bc1785dc04e03f975be087a
Anmizi <1845513904@qq.com>
2022-01-28 14:24:17 +0800

    引入vuex模块

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 
 // 导入TypeNav组件
 import TypeNav from './pages/Home/TypeNav'
@@ -10,5 +11,7 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
+  // 注册仓库：组件实例身上会多出一个$store属性
+  store,
   render: h => h(App),
 }).$mount('#app')
