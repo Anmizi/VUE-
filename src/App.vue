727a07e6ee2269a596d48a0560072cc3ab5bb489
Anmizi <1845513904@qq.com>
2022-02-08 16:57:35 +0800

    优化TypeNav组件数据获取

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <template>
   <div>
-    <Header/>
+    <Header />
     <router-view></router-view>
-    <Footer/>
+    <Footer v-show="$route.meta.show" />
   </div>
 </template>
 
 <script>
-import Header from './components/Header'
-import Footer from './components/Footer'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 export default {
   name: "App",
   components: {
     Header,
-    Footer
+    Footer,
+  },
+  mounted(){
+    // app.vue根组件只执行并挂载一次，在次请求数据，防止组件切换多次请求数据
+    this.$store.dispatch('categoryList')
   }
 };
 </script>
