31d40e2a45c5ae83147a77fb360d261758ba6f22
Anmizi <1845513904@qq.com>
2022-02-11 18:08:04 +0800

    完成search模板动态渲染数据

--- src/store/search/index.js
+++ src/store/search/index.js
@@ -6,7 +6,7 @@ const state = {
 
 // mutations:修改state的唯一手段
 const mutations = {
-  SEARCHINFO(state,searchList){
+  SEARCHINFO(state, searchList) {
     state.searchList = searchList
   }
 }
@@ -16,13 +16,25 @@ const actions = {
   async getSearchInfo({ commit }, data = {}) {
     const res = await reqGetSearchInfo(data)
     if (res.code === 200) {
-      commit('SEARCHINFO',res.data)
+      commit('SEARCHINFO', res.data)
     }
   }
 }
 
 //getters: 理解为计算属性，用于简化仓库数据的获取
-const getters = {}
+const getters = {
+  trademarkList(state) {
+    // 当网络不好的时候，可能返回undefined,会出错
+    // 若为undefined 返回空数组
+    return state.searchList.trademarkList || []
+  },
+  attrsList(state) {
+    return state.searchList.attrsList || []
+  },
+  goodsList(state) {
+    return state.searchList.goodsList || []
+  }
+}
 
 export default {
   state,
